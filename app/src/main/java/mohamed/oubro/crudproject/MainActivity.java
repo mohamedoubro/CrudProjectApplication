3098ffa22152975093e6702e6df5495aabb898bd
Mohamed Oubro <ouratwain@gmail.com>
2023-02-07 21:50:43 +0100

    organize the repo and project

--- app/src/main/java/mohamed/oubro/crudproject/MainActivity.java
+++ app/src/main/java/mohamed/oubro/crudproject/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void connect(){
+        Connection connection = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/crudproject";
+            connection = DriverManager.getConnection(url, "admin", "");
+            System.out.println("You are connected!");
+        }
+        catch(Exception e){
+            System.out.println("Message : "+e);
+        }
     }
 }
---
+++ app/src/main/java/mohamed/oubro/crudproject/ShowData.java
@@ -0,0 +1,14 @@
+package mohamed.oubro.crudproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showdata);
+    }
+}
